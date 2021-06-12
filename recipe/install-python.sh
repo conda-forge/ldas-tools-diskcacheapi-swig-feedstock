5a63c38d3a9b3e3106518935b4dec6939363ec7d
Duncan Macleod <duncan.macleod@ligo.org>
2021-06-12 14:57:13 +0100

    update cmake usage

--- recipe/install-python.sh
+++ recipe/install-python.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 set -ex
-pushd ${SRC_DIR}/build
+mkdir -p _build${PYVER}
+cd _build${PYVER}
 
-# get python options
 if [ "${PY3K}" -eq 1 ]; then
-  PYTHON_BUILD_OPTS="-DENABLE_SWIG_PYTHON2=no -DENABLE_SWIG_PYTHON3=yes -DPYTHON3_EXECUTABLE=${PYTHON}"
 else
   PYTHON_BUILD_OPTS="-DENABLE_SWIG_PYTHON3=no -DENABLE_SWIG_PYTHON2=yes -DPYTHON2_EXECUTABLE=${PYTHON}"
 fi
 
 # configure
-cmake .. \
-	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
+cmake \
+	${SRC_DIR} \
+	${CMAKE_ARGS} \
 	-DCMAKE_BUILD_TYPE=Release \
-	${PYTHON_BUILD_OPTS}
+	-DENABLE_SWIG_PYTHON2=no \
+	-DENABLE_SWIG_PYTHON3=yes \
+	-DPYTHON3_EXECUTABLE="${PYTHON}" \
+;
 
 # build
-cmake --build python -- -j ${CPU_COUNT}
+cmake --build python --parallel ${CPU_COUNT} --verbose
 
 # check
-ctest -VV
+ctest --test-dir python --parallel ${CPU_COUNT} --verbose
 
 # install
-cmake --build python --target install
+cmake --build python --parallel ${CPU_COUNT} --verbose --target install
