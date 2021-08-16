ca974baf6e07783ad31877f73395d2ba94716e07
Duncan Macleod <duncan.macleod@ligo.org>
2021-08-16 14:03:17 +0100

    update recipe

--- recipe/install-python.sh
+++ recipe/install-python.sh
@@ -4,19 +4,14 @@ set -ex
 mkdir -p _build${PYVER}
 cd _build${PYVER}
 
-if [ "${PY3K}" -eq 1 ]; then
-else
-  PYTHON_BUILD_OPTS="-DENABLE_SWIG_PYTHON3=no -DENABLE_SWIG_PYTHON2=yes -DPYTHON2_EXECUTABLE=${PYTHON}"
-fi
-
 # configure
 cmake \
 	${SRC_DIR} \
 	${CMAKE_ARGS} \
-	-DCMAKE_BUILD_TYPE=Release \
-	-DENABLE_SWIG_PYTHON2=no \
-	-DENABLE_SWIG_PYTHON3=yes \
-	-DPYTHON3_EXECUTABLE="${PYTHON}" \
+	-DCMAKE_BUILD_TYPE:STRING=Release \
+	-DENABLE_SWIG_PYTHON2:BOOL=no \
+	-DENABLE_SWIG_PYTHON3:BOOL=yes \
+	-DPYTHON3_EXECUTABLE:FILE="${PYTHON}" \
 ;
 
 # build
